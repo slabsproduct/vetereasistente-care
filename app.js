0a49e5564989caf229f703b1e671f9f6c57cfa22
slabsproduct <slabsproduct@gmail.com>
2026-03-10 20:08:29 -0300

    inicio del proyecto vetereasistente-care

---
+++ app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🐾 Servidor iniciado en el puerto ${PORT}`);
+});
